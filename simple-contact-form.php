d8880480cb99077ab4c77e27a8fcf7c97c38c6ce
mikejeter <mjeter45@yahoo.com>
2021-08-13 14:46:12 -0400

    Created new files for plugin index.php, simple-contact-form.php

---
+++ simple-contact-form.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: Simple Contact Form 
+ * Description: Simple email contact form
+ * Author: Michael Jeter
+ * Author URI: https:myhoststream.net
+ * Version: 1.0.0
+ * Text Domain: simple-contact-form
+ */
+
+ if( !defined('ABSPATH')) {
+    exit;
+ }
+
+ class SimpleContactForm {
+
+    public function __construct() 
+    {
+        
+    }
+
+
+ }
+
+ new SimpleContactForm;
